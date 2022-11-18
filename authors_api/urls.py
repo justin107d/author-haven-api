60487aa2f46137356f962237fa54383b044f4703
justin107d <justin107d@gmail.com>
2022-11-18 14:47:41 -0500

    finished setting up djoser and jwt auth

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -22,9 +22,8 @@ urlpatterns = [
     # creates documentation using open API
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
-    
-    # path("api/v1/auth/", include("djoser.urls")),
-    # path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
     # path("api/v1/profiles/", include("core_apps.profiles.urls")),
     # path("api/v1/articles/", include("core_apps.articles.urls")),
     # path("api/v1/ratings/", include("core_apps.ratings.urls")),
